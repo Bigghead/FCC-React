40ce3fd073ce549969e2bdaffe18cfd75a1e21a6
Shav <shavenchew@yahoo.com>
2017-04-13 20:48:55 -0700

    Successful light up on element click

--- FCC-Simon/app.js
+++ FCC-Simon/app.js
@@ -2,23 +2,34 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-
+            lit: 0
         };
     }
+
+    lightUp(num){
+        this.setState({
+            lit: num
+        }, setTimeout(() => {
+         this.setState({
+             lit: 0
+         })   
+        }, 500))
+    }
     render() {
+
         return (
             <div>
                 <div className="container">
-                        <div className="firstRow">
-                            <div className="box red">Hello</div>
-                        <div className="box red">Hello</div>
-                        </div>
+                    <div className="firstRow">
+                        <div className={classNames('box red faded', {lit: this.state.lit === 1})}onClick={ () => this.lightUp(1) }></div>
+                        <div className={classNames('box blue faded', {lit: this.state.lit === 2})}onClick={ () => this.lightUp(2) }></div>
+                    </div>
 
-                        <div className="secondRow">
-                            <div className="box red">Hello</div>
-                        <div className="box red">Hello</div>
-                        </div>
-                        
+                    <div className="secondRow">
+                        <div className={classNames('box green faded', {lit: this.state.lit === 3})}onClick={ () => this.lightUp(3) }></div>
+                        <div className={classNames('box yellow faded', {lit: this.state.lit === 4})}onClick={ () => this.lightUp(4) }></div>
+                    </div>
+
                 </div>
             </div>
         );
