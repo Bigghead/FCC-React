bfb408887bcda25f051d27753d7e28baec154349
Shav <shavenchew@yahoo.com>
2017-04-13 21:24:36 -0700

    Successful sequenced animation

--- FCC-Simon/app.js
+++ FCC-Simon/app.js
@@ -2,23 +2,41 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            lit: 0
+            lit: 0,
+            lighting: false,
+            gameSequence: [2, 3, 1, 4, 3]
         };
     }
 
     lightUp(num){
         this.setState({
             lit: num
-        }, setTimeout(() => {
+        }, () => {
+            setTimeout(() => {
          this.setState({
              lit: 0
          })   
-        }, 500))
+        }, 500)
+        })
+    }
+
+    gameStart(){
+        let i = 0;
+        var interval = setInterval(() => {
+            // let random = Math.floor(Math.random() * 4) + 1;
+            // console.log(random);
+            this.lightUp(this.state.gameSequence[i]);
+            i++;
+            if(i === this.state.gameSequence.length - 1){
+                clearInterval(interval);
+            } 
+        }, 700);
     }
     render() {
 
         return (
             <div>
+                <button onClick={() => this.gameStart()}>Start</button>
                 <div className="container">
                     <div className="firstRow">
                         <div className={classNames('box red faded', {lit: this.state.lit === 1})}onClick={ () => this.lightUp(1) }></div>
